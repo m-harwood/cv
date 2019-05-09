d20540df5be6aebeb129bfab580feb313938ee4f
Mark Harwood <m-harwood@hotmail.com>
2019-05-09 21:46:42 +0000

    Added interests & git pages with Maps

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 50.819413, lng: -0.136474 },
+        { lat: 50.802361, lng: -0.540863 },
+        { lat: 51.373701, lng: -0.100311 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
