08031ef868971cd6faf0a70673450c8fe552e91c
Mark Harwood <m-harwood@hotmail.com>
2019-05-11 10:00:52 +0000

    Render user info from GitHub

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 8,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 51.304726,
+            lng: -0.071999
         }
     });
 
